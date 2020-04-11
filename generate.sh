518a35bcaa85402625a80637861346d236f9a88e
klim0v <klim0v-sergey@yandex.ru>
2020-04-11 14:04:16 +0300

    add mkdir

--- generate.sh
+++ generate.sh
@@ -2,10 +2,12 @@
 
 cd "$(dirname "$0")" || exit
 
+mkdir -p "./api_pb"
+
 protoc -I/usr/local/include -I. \
     -I"$GOPATH"/src \
     -I"$GOPATH"/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
     -I"$GOPATH"/src/github.com/grpc-ecosystem/grpc-gateway \
     --grpc-gateway_out=logtostderr=true:./api_pb \
-     --swagger_out=allow_merge=true,merge_file_name=api:. \
-     --go_out=plugins=grpc:./api_pb ./*.proto
+    --swagger_out=allow_merge=true,merge_file_name=api:. \
+    --go_out=plugins=grpc:./api_pb ./*.proto
