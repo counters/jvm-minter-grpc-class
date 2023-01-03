2cb59d8e1ecb6574e86683d5db314589af6c9097
webcounters <webcounters@gmail.com>
2023-01-03 11:46:16 +0300

    Added native generation from proto files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,33 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
 
 plugins {
     kotlin("jvm") version "1.5.20"
 //    application
+    id("com.google.protobuf") version "0.9.1"
+    `maven-publish`
 }
-val grpcKotlinVersion = "1.2.1"
-val coroutinesVersion = "1.6.1"
+//val grpcKotlinVersion = "1.3.0"
+//val coroutinesVersion = "1.6.4"
 
-
-group = "com.webcounters"
-version = "v1.1.0"
+group = "counters"
+version = "1.2.1"
 
 repositories {
-//    mavenLocal()
     mavenCentral()
 }
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("com.google.protobuf:protobuf-java:3.20.0")
-    implementation("com.google.protobuf:protobuf-kotlin:3.20.0")
+    implementation("com.google.protobuf:protobuf-java:3.21.12")
+    implementation("com.google.protobuf:protobuf-kotlin:3.21.12")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-    implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("io.grpc:grpc-kotlin-stub:1.3.0")
 
-    implementation ("io.grpc:grpc-netty-shaded:1.45.1")
-    implementation ("io.grpc:grpc-protobuf:1.45.1")
-    implementation ("io.grpc:grpc-stub:1.45.1")
+    implementation ("io.grpc:grpc-netty-shaded:1.50.2")
+    implementation ("io.grpc:grpc-protobuf:1.50.2")
+    implementation ("io.grpc:grpc-stub:1.50.2")
 }
 
 tasks.test {
@@ -37,6 +38,52 @@ tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-/*application {
+publishing {
+    repositories {
+        maven {
+            name = "jvm-minter-grpc-class"
+            url = uri("https://maven.pkg.github.com/counters/jvm-minter-grpc-class")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.20.0"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.50.2"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.3.0:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
+    generatedFilesBaseDir = "$projectDir/src/generated"
+}
+
+/*
+application {
     mainClass.set("MainKt")
-}*/
+}
+*/
